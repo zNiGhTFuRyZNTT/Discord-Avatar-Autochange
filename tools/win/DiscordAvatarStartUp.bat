b4635484531571b96bc351669e2aa7157b251606
zNiGhTFuRyZNTT <78607877+zNiGhTFuRyZNTT@users.noreply.github.com>
2021-12-15 20:43:01 +1100

    Fix Directory bug 💖

--- tools/win/DiscordAvatarStartUp.bat
+++ tools/win/DiscordAvatarStartUp.bat
@@ -1,10 +1,12 @@
 @echo off
 timeout /t 30
 
+@rem F:
 @rem cd path_to_project_root_directory
 @rem py path_to_main.py
 
 
-@rem uncomment the top 2 lines and replace the required paths
-@rem Example cd F:\Github\Discord-Avatar-Autochange
-@rem Example: "py F:\Github\Discord-Avatar-Autochange\main.py"
+@rem uncomment the top 3 lines and replace the required paths
+@rem 1- change F: to the drive letter your project is in.
+@rem 2- Example cd F:\Github\Discord-Avatar-Autochange
+@rem 3- Example: "py F:\Github\Discord-Avatar-Autochange\main.py"
