86fa4555c7b13a1a8a45051e1edfc67684f89a63
zNiGhTFuRyZNTT <78607877+zNiGhTFuRyZNTT@users.noreply.github.com>
2021-12-15 16:53:12 +1100

    Clearify

--- tools/DiscordAvatarStartUp.bat
+++ tools/DiscordAvatarStartUp.bat
@@ -1,3 +1,6 @@
 @echo off
 timeout /t 30
-@rem Delete this line and pase "py <path_to_main.py>" for windows and "python3 <path_to_main.py>"
+@rem python3 <path_to_main.py>  @Rem<--- Uncomment this line and replace path!
+
+
+@rem Example: "python3 /home/test/Desktop/DiscordAvatar/main.py"
--- tools/terminateDis.bat
+++ tools/terminateDis.bat
@@ -3,3 +3,4 @@ for /f "tokens=*" %%s in (pid.txt) do (
   taskkill /F /PID %%s
 )
 @Rem Add create a shortcut of this file to stop the program after start up.
+@Rem or if using linux add this to bath so you can terminate the python job anytime
